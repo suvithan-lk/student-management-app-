9fc2d622ce9f62cc4211dbcc21d5fcf6f3e444b0
A_Suvithan <suvithan.lk@gmail.com>
2026-09-09 13:49:01 +0530

    Add validation attributes to CreateStudentDto

--- StudentCrudApp/Application/DTOs/CreateStudentDto.cs
+++ StudentCrudApp/Application/DTOs/CreateStudentDto.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace StudentCrudApp.Application.DTOs
 {
     public class CreateStudentDto
     {
+        [Required, StringLength(100, MinimumLength = 2)]
         public string Name { get; set; } = string.Empty;
+
+        [Required, EmailAddress, StringLength(150)]
         public string Email { get; set; } = string.Empty;
+
+        [Required, Phone, StringLength(20)]
         public string Phone { get; set; } = string.Empty;
+
+        [Required, StringLength(100, MinimumLength = 2)]
         public string Course { get; set; } = string.Empty;
+
+        [Required, DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
     }
 }
