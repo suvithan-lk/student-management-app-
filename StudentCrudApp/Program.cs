a150763aeda939cd485a4ccd738a10f9a018ba52
A_Suvithan <suvithan.lk@gmail.com>
2026-09-09 13:52:28 +0530

    Improve production migration safety

--- StudentCrudApp/Program.cs
+++ StudentCrudApp/Program.cs
@@ -6,29 +6,27 @@ using StudentCrudApp.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Database
 builder.Services.AddDbContext<StudentDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Dependency Injection
 builder.Services.AddScoped<IStudentRepository, StudentRepository>();
 builder.Services.AddScoped<StudentService>();
 
 var app = builder.Build();
 
-// Apply migrations and create database
-using (var scope = app.Services.CreateScope())
+// Apply migrations automatically only in development.
+// Production migrations should be executed as part of deployment.
+if (app.Environment.IsDevelopment())
 {
+    using var scope = app.Services.CreateScope();
     var dbContext = scope.ServiceProvider.GetRequiredService<StudentDbContext>();
     dbContext.Database.Migrate();
 }
 
-// Configure middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
